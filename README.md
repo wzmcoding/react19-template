22038823df03d7717c8813b95e9f7c26805061a9
wzm <2791063637@qq.com>
2025-05-09 17:10:02 +0800

    feat: add dialog, form, input, and label components for login functionality
    - Implemented a reusable Dialog component for modal interactions.
    - Created Form, FormField, FormItem, FormLabel, FormControl, FormDescription, and FormMessage components to handle form state and validation.
    - Developed Input and Label components for user input and labeling.
    - Integrated the new components into the login view, replacing the previous card layout with a dialog-based form for user login.
    - Added validation using zod and react-hook-form for phone number and code inputs.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# React19 + TypeScript + Vite  + ahooks + react-hook-form + @tanstack/react-query + typescript + tailwindcss + zustand + react-router +  shadcn/ui
+# React19 + TypeScript + Vite  + ahooks + react-hook-form + zod + @tanstack/react-query + typescript + tailwindcss + zustand + react-router +  shadcn/ui
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,33 +1,97 @@
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+import { useState } from "react";
 import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+} from "@/components/ui/dialog"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 import LoginImg from "@/assets/login.png"
 
 const Login = () => {
+    const [open, setOpen] = useState(false);
+    const formSchema = z.object({
+        phone: z.string().min(11, {
+            message: "phone must be at least 11 characters.",
+        }),
+        code: z.string().min(6, {
+            message: "code must be at least 6 characters.",
+        }),
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            phone: "",
+            code: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log('%c [ values ]-40', 'font-size:13px; background:pink; color:#bf2c9f;', values)
+    }
     return (
         <div className="w-[100vw] h-[100vh] bg-secondary flex justify-center items-center">
-            <Card className="w-200 h-120 flex items-center justify-around flex-row p-14">
-                <div className="flex-1">
-                    <img src={LoginImg} alt="login" className="w-89 h-72.5" />
-                </div>
-                <div className="flex-1 w-full h-full mt-15 flex flex-col gap-8">
-                    <CardHeader>
-                        <CardTitle>登录OI Practice</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
-                    </CardContent>
-                    <CardFooter>
-                        <Button variant="default" className="bg-primary">登录</Button>
-                    </CardFooter>
-                </div>
-            </Card>
-
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="min-w-216 h-130">
+                    <div className="w-200 h-120 flex items-center justify-around flex-row p-14 gap-3">
+                        <div className="flex-1">
+                            <img src={LoginImg} alt="login" className="w-89 h-72.5" />
+                        </div>
+                        <div className="flex-1 w-full h-full mt-18 flex flex-col gap-8">
+                            <div className="font-semibold text-2xl">登录 OI Practice</div>
+                            <div>
+                                <Form {...form}>
+                                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                                        <FormField
+                                            control={form.control}
+                                            name="phone"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormControl>
+                                                        <Input placeholder="请输入手机号" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <div className="flex items-start justify-between gap-3">
+                                            <div className="flex-1">
+                                                <FormField
+                                                    control={form.control}
+                                                    name="code"
+                                                    render={({ field }) => (
+                                                        <FormItem>
+                                                            <FormControl>
+                                                                <Input placeholder="请输入验证码" {...field} />
+                                                            </FormControl>
+                                                            <FormMessage />
+                                                        </FormItem>
+                                                    )}
+                                                />
+                                            </div>
+                                            <Button>验证码</Button>
+                                        </div>
+                                        <Button type="submit" variant="default" className="bg-primary w-80 py-6 text-xl rounded-full">登录</Button>
+                                    </form>
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
+            <Button onClick={() => setOpen(true)}>去登录</Button>
         </div>
     );
 }
