346bc594692f1cbad7b7a759094aabe6a524f8d8
wzm <2791063637@qq.com>
2025-05-26 17:05:09 +0800

    feat: 实现主页右侧用户组件

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import imgUser from '@/assets/user.svg';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
 
 const User = () => {
     return (
-        <div>
-            User
+        <div className="h-full w-90">
+            <div className="flex justify-end pr-10 pt-2">
+                <Popover>
+                    <PopoverTrigger>
+                        <img className="h-15 w-15 border rounded-full cursor-pointer" src={imgUser} />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <div className='cursor-pointer hover:bg-primary/20 hover:text-primary p-2'>退出登录</div>
+                    </PopoverContent>
+                </Popover>
+            </div>
         </div>
     );
 }
